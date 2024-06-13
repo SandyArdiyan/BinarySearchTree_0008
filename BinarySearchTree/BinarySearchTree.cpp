8013d04ecc49d35a99ab55919c13dd9e1348491b
SandyArdiyan <sanardiyan881@gmail.com>
2024-06-13 20:12:33 +0700

    12

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -133,4 +133,12 @@ int main()
 
         switch (ch)
         {
- 
+        case '1':
+        {
+            cout << "Enter a word: ";
+            string word;
+            cin >> word;
+            obj.insert(word);
+            break;
+        }
+       
